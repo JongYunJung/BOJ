2452a6c27d520e309958168ebf1471b145370191
janghyoseog <bestdevhyo1225@gmail.com>
2019-03-24 00:01:25 +0900

    분산 처리

--- 1000/1009.cpp
+++ 1000/1009.cpp
@@ -1,6 +1,5 @@
 // BOJ 1009 분산 처리
 // 각 숫자에서 제곱을 진행할 때마다 일의 자리수의 패턴이 존재한다.
-// 이 핵심을 가지고 문제를 해결했음
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,14 +9,14 @@ int main() {
     cin.tie(0);
     
     // 각 숫자에서 제곱시 나오는 패턴 미리 구하기
-    vector<int> numOfCase[10];
-    numOfCase[0].push_back(10);
+    vector<int> pattern[10];
+    pattern[0].push_back(10);
     for (int num = 1; num < 10; num++) {
-        numOfCase[num%10].push_back(num % 10);
-        int temp = num * num;
-        while (temp % 10 != (num % 10)) {
-            numOfCase[num%10].push_back(temp % 10);
-            temp *= num;
+        pattern[num].push_back(num);
+        int tempNum = num * num;
+        while (tempNum % 10 != num) {
+            pattern[num].push_back(tempNum % 10);
+            tempNum *= num;
         }
     }
     
@@ -26,9 +25,11 @@ int main() {
     while (tc--) {
         int a, b;
         cin >> a >> b;
-        int cSize = (int)numOfCase[a%10].size();
-        if (b % cSize == 0) cout << numOfCase[a%10][cSize-1] << '\n';
-        else cout << numOfCase[a%10][(b%cSize)-1] << '\n';
+        a %= 10;
+        int pSize = (int)pattern[a].size();
+        if (b % pSize == 0) cout << pattern[a][pSize-1];
+        else cout << pattern[a][b%pSize-1];
+        cout << '\n';
     }
     
     return 0;
