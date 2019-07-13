34bf70688d60ab719d1a250bbd33e14b942b080d
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-13 19:14:04 +0900

    최소 편집

--- 15400/15438.cpp
+++ 15400/15438.cpp
@@ -7,21 +7,25 @@
 
 using namespace std;
 
-int cache[1001][1001];
 string src, dst;
+int cache[1001][1001];
 
-int go(int row, int col) {
-    if (row < 0 && col < 0) return 0;
-    if (cache[row][col] != -1) return cache[row][col];
-    int& result = cache[row][col] = 0;
-    if (dst[row] == src[col]) result = go(row - 1, col - 1);
-    else result = min(go(row - 1, col), min(go(row, col - 1), go(row - 1, col - 1))) + 1;
+int go(int r, int c) {
+    if (r < 0 && c < 0) return 0;
+    if (cache[r][c] != -1) return cache[r][c];
+    
+    int& result = cache[r][c] = 0;
+    if (src[r] == dst[c]) result = go(r-1, c-1);
+    else result = 1 + min(min(go(r-1, c), go(r, c-1)), go(r-1, c-1));
+    
     return result;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    
+    memset(cache, -1, sizeof(cache));
     
     cin >> src >> dst;
     
@@ -31,12 +35,10 @@ int main() {
     int srcSize = (int)src.size();
     int dstSize = (int)dst.size();
     
-    memset(cache, -1, sizeof(cache));
+    for (int i = 0; i < srcSize; i++) cache[i][0] = i;
+    for (int i = 0; i < dstSize; i++) cache[0][i] = i;
     
-    for (int i = 0; i < srcSize; i++) cache[0][i] = i;
-    for (int i = 0; i < dstSize; i++) cache[i][0] = i;
-    
-    cout << go(dstSize - 1, srcSize - 1) << '\n';
+    cout << go(srcSize-1, dstSize-1) << '\n';
     
     return 0;
 }
