cfdad258656f0a60a1ec8f98c9778b33c0157865
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-14 13:55:23 +0900

    백대열

---
+++ 14400/14490.cpp
@@ -0,0 +1,37 @@
+// BOJ 14490 - 백대열
+// 최대 공약수의 값으로 값들을 나눠주면 된다.
+// 최대 공약수는 '유클리드 호제법'을 이용해서 구한다.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int GCD(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    string s;
+    cin >> s;
+    
+    size_t index = s.find(':');
+    int a = stoi(s.substr(0, index));
+    int b = stoi(s.substr(index + 1, s.length() - (index + 1)));
+    
+    int tempA = a, tempB = b;
+    if (tempA < tempB) swap(tempA, tempB);
+    int gcd = GCD(tempA, tempB);
+    
+    string answer = to_string(a / gcd) + ":" + to_string(b / gcd);
+    cout << answer << '\n';
+    
+    return 0;
+}
