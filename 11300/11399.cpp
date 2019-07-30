d00be1f54a5716e6d36fbdda36a8687770c89391
janghyoseog <bestdevhyo1225@gmail.com>
2019-07-30 09:42:34 +0900

    ATM

--- 11300/11399.cpp
+++ 11300/11399.cpp
@@ -36,3 +36,36 @@ int main() {
     
     return 0;
 }
+
+// BOJ 11399 - ATM
+// 190730 - 두 번째 풀이
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    int temp = 0;
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        temp += arr[i];
+        answer += temp;
+    }
+    
+    cout << answer << '\n';
+    return 0;
+}
