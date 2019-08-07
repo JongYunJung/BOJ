1c0b38ba1b3a65389e3d646ed7b0581562c0e492
janghyoseog <bestdevhyo1225@gmail.com>
2019-08-07 16:50:20 +0900

    개똥 벌레

--- 3000/3020.cpp
+++ 3000/3020.cpp
@@ -49,3 +49,68 @@ int main() {
     cout << a[1] << " " << cnt << '\n';
     return 0;
 }
+
+// 이분 탐색 풀이
+// lower_bound, upper_bound 직접 구현해봄
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int my_lower_bound(vector<int>& arr, int key) {
+    int start = 0, end = (int)arr.size();
+    while (start < end) {
+        int mid = (start + end) / 2;
+        if (arr[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return end;
+}
+
+int my_upper_bound(vector<int>& arr, int key) {
+    int start = 0, end = (int)arr.size();
+    while (start < end) {
+        int mid = (start + end) / 2;
+        if (arr[mid] <= key) {
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return end;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, h;
+    cin >> n >> h;
+    
+    vector<int> bot(n/2), top(n/2);
+    for (int i = 0; i < n/2; i++) {
+        cin >> bot[i] >> top[i];
+    }
+    
+    sort(bot.begin(), bot.end());
+    sort(top.begin(), top.end());
+    
+    int answer = n, cnt = 0;
+    for (int i = 1; i <= h; i++) {
+        int temp = (int)bot.size() - my_lower_bound(bot, i);
+        temp += (int)top.size() - my_upper_bound(top, h - i);
+        
+        if (answer == temp) cnt++;
+        else if (answer > temp) {
+            answer = temp;
+            cnt = 1;
+        }
+    }
+    
+    cout << answer << " " << cnt << '\n';
+    return 0;
+}
